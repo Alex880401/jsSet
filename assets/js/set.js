6fcac145b7eba2c6e35326fbb6d0213bcdb68242
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-10-15 17:20:45 +0300

    Make 1st part

---
+++ assets/js/set.js
@@ -0,0 +1,17 @@
+// 1.
+
+const testSet1 = new Set([1, 2, 3]);
+
+console.log(testSet1.has(3));
+
+console.log(testSet1.has(4));
+
+testSet1.add(4).add(5).add(0);
+console.log(testSet1);
+
+for (let item of testSet1) {
+    console.log(item);
+};
+
+const sum = [...testSet1].reduce((accum, current) => accum + current, 0);
+console.log(sum);
